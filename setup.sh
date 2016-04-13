463fbf0dfdd6084711758307c82e353c07b8491b
Omar Zapata <Omar.Zapata@cern.ch>
2016-04-12 20:49:54 -0500

    - added delphes from github - fixes in setup.sh for the new configuration

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-export DELPHES=$HOME/external/delphes
+export DELPHES=$PWD/delphes
 export PATH=$PWD/bin:$PATH
-export FASTJET=$HOME/external
+export FASTJET=$PWD/delphes/external
 export DYLD_LIBRARY_PATH=$FASTJET/lib
-export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$PWD/python:$PYTHONPATH
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH:$DELPHES
+export PYTHONPATH=$PWD/python:$PYTHONPATH:$DELPHES/python
 
 
 
